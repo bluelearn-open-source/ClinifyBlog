94afa68f1736c345d0a6677116d16732e6fe93b5
Yog <yog.panjarale@gmail.com>
2021-08-02 22:07:50 +0530

    api route for blog done

--- frontend/pages/api/blog/[slug].ts
+++ frontend/pages/api/blog/[slug].ts
@@ -1,9 +1,28 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
+import admin from '../../../src/firebase/admin';
 type Data = {
-  name: string
+  slug: string,
+  content?: any,
+  error?: string
 }
 
-export default (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  res.status(200).json({ name: 'John Doe' })
+export default async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const firestore = admin.firestore();
+  const slug = req.query.slug as string;
+  try {
+
+    const doc = await firestore.collection('blogs').where("slug", "==", slug).get();
+    const blog = doc.docs[0].data();
+    if (blog.user) {
+      blog.user = (await firestore.collection('users').doc(blog.user).get()).data();
+    }
+    if (blog.time) {
+      blog.time = new Date(blog.time._seconds * 1000);
+    }
+    console.log(blog)
+    res.status(200).json({ slug: slug, content: blog })
+  } catch (error) {
+    res.status(404).json(
+      { error: error.message == "Cannot read property 'data' of undefined" ? "blog not found" : error.message, slug: slug });
+  }
 }
--- frontend/src/firebase/admin.ts
+++ frontend/src/firebase/admin.ts
@@ -2,9 +2,11 @@ import * as admin from 'firebase-admin';
   
 var serviceAccount =JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT as string)
 // console.log(serviceAccount,typeof serviceAccount);
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://farm-helper-app-mqtt.firebaseio.com/"
-});
+if (admin.apps.length<=0){
+    admin.initializeApp({
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://farm-helper-app-mqtt.firebaseio.com/"
+    });
+}
 
 export default admin
